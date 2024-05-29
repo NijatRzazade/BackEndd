a5ca8357835da32b0f2091fa0f350b13f1326eba
NijatRzazade <rzazadenicat009@gmail.com>
2024-05-29 19:29:44 +0400

    Lesson-2

---
+++ Lesson-2(again)/Lesson-2(again)/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2_again_
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//string soz = "Salam Dünya";
+			//Console.WriteLine(soz);
+			//Console.WriteLine(soz);
+			//Console.WriteLine(soz);
+			//Console.WriteLine(soz);
+			//Console.WriteLine(soz);
+			//Console.WriteLine(soz);
+			//Console.WriteLine(soz);
+			//Console.WriteLine("hello word");
+
+			/*byte age = 28;*/ //255
+			/*sbyte age2 = -28;*/ //-128, //127
+
+			//int x = 250;
+			//long y = 2555555555555555;
+
+			//Console.WriteLine(age + 20); 
+
+			// Task
+			//Console.WriteLine("Zehmet olmazsa Adınızı Soyadınızı daxil edin");
+			//string fullname=Console.ReadLine();
+			//Console.WriteLine("Salam"+ " " +fullname);
+
+			//Console.WriteLine("Zehmet olmazsa Yaşınızı daxil edin");
+			//byte age =Convert.ToByte(Console.ReadLine());
+			//Console.WriteLine("Yaş: "+ age);
+
+			//Console.WriteLine("Zehmet olmazsa Adınızı Soyadınızı daxil edin");
+			//string fullname = Console.ReadLine();
+			//Console.WriteLine("Zehmet olmazsa Yaşınızı daxil edin");
+			//byte age = Convert.ToByte(Console.ReadLine());
+			//Console.WriteLine(fullname + " " + age);
+
+			//Task2
+			Console.WriteLine("1-ci ededi daxil edin:");
+			int x1 =Convert.ToInt32( Console.ReadLine());
+			Console.WriteLine("2-ci ededi daxil edin:");
+			int x2=Convert.ToInt32( Console.ReadLine());
+			Console.WriteLine("Cavab:");
+			int Cvb = x1 + x2;
+			Console.WriteLine(x1 + x2);
+		}
+	}
+}

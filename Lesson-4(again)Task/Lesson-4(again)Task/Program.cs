e91c172ab79320e8782a43f4476e4dad3cbb1ca8
NijatRzazade <rzazadenicat009@gmail.com>
2024-05-31 15:50:48 +0400

    Lesson-4 Task

---
+++ Lesson-4(again)Task/Lesson-4(again)Task/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4_again_Task
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arr = { 4, 5, 2, 1, 5, 2, 5, 3, 5, 6, 2, 1, 6, 2, 6, 2, 5, 3, 2, 7, 4, 6, 4, 5, 6, 2, 5, 6, 3, 7, 3, 7 };
+
+			//arraydaki ilk 3 e bolunende qaliqda 2 alinan reqemi ve hemin reqemin arraydaki indexini tapin
+			//int x = 0;
+			//for(int i=0; i < arr.Length; i++)
+			//{
+			//	if (arr[i] % 3 == 2)
+			//	{
+			//		x++;
+			//		Console.WriteLine(arr[i]);
+   //             }
+			//	if (x == 3)
+			//	{
+			//		break;
+			//	}
+			//}
+
+			//5 reqeminin arraydaki en boyuk ve en kicik indexlerini tapin
+			//int[]array=new int[arr.Length];
+			//int count = 0;
+			//for(int i=0; i < arr.Length; i++)
+			//{
+			//	if (arr[i] == 5)
+			//	{
+			//		array[count]=i; 
+			//		count++;
+			//	}
+			//}
+			//Console.WriteLine("Min: " + array[0]);
+			//Console.WriteLine("Max: " + array[count-1]);
+
+
+		}
+	}
+}
